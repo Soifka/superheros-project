a63c49e34e0772900b463f0935ba3734b0c55bf1
Julia Alekseeva <soifka@gmail.com>
2023-02-19 00:05:07 +0200

    Add Superpower middleware

---
+++ middlewares/superpower.mv.js
@@ -0,0 +1,18 @@
+const SuperpowerError = require('../errors/SuperpowerError');
+const { Star, Superpower } = require('../models/');
+
+module.exports.findSuperpower = async(req, res, next) => {
+    try {
+        const { params: {superpowerId} } = req;
+        console.log(req)
+        const superpower = await Superpower.findByPk(superpowerId);
+        if(superpower) {
+            req.superpowerInstance = superpower;
+            next();
+        } else {
+            throw new SuperpowerError(404, `Cannot find superpower with id ${superpowerId}`);
+        }
+    } catch (error) {
+        next(error);
+    }
+};
