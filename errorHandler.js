d154110bf71a3404ac57316544f7d4e0eeb0dfd6
Julia Alekseeva <soifka@gmail.com>
2023-02-19 13:27:06 +0200

    Fix errorHandler

--- errorHandler.js
+++ errorHandler.js
@@ -1,17 +1,16 @@
 const StarError = require("./errors/StarError");
 const GlobalError = require("./errors/GlobalError");
+const SuperpowerError = require("./errors/SuperpowerError");
 const { ValidationError } = require('yup');
 
+
 module.exports.errorHandler = async(err, req, res, next) => {
-    if(err instanceof StarError) {
-        return res.status(err.code).send(err.message);
-    } else if(err instanceof GlobalError) {
-        console.log((err))
+    if(err instanceof StarError || err instanceof SuperpowerError || err instanceof GlobalError) {
         return res.status(err.code).send(err.message);
     } else if(err instanceof ValidationError) {
         return res.status(400).send(`Check the entered data; ${err.errors}`)
     } else {
-        // console.log(err)
-        return res.status(err.status).send(`Something is wrong. Error type: ${err.type}`);
+        console.log(err)
+        return res.status(err.status ? err.status : 400).send(`Something is wrong: ${err.name}`);
     }
 } 
